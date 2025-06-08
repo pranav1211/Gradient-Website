27e704b9b525b4fc7c69a05e6bc71b005f0af4e6
pranavisda1 <pranavisda1@gmail.com>
2025-06-09 02:24:51 +0530

    9/6

--- app/utsav25/page.tsx
+++ app/utsav25/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 const RedirectOnLoad: React.FC = () => {
   useEffect(() => {
     // Redirect to an external URL on component load
-    window.location.href = "https://gradient-week.vercel.app";
+    window.location.href = "https://gradient-x-utsav25-special-page.vercel.app/";
   }, []);
 
   return null; // No UI needed since this component just redirects
