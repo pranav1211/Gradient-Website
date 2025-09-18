eff81e0b3c232876643613941778a2a974ce10fc
pranavisda1 <pranavisda1@gmail.com>
2025-09-19 01:03:45 +0530

    19-09-2025

---
+++ app/bfs/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React, { useState, useCallback, useEffect } from "react";
+
+const FullscreenImage: React.FC = () => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const toggleFullscreen = useCallback(() => {
+    const elem = document.documentElement;
+
+    if (!document.fullscreenElement) {
+      elem.requestFullscreen?.().then(() => setIsFullscreen(true));
+    } else {
+      document.exitFullscreen?.().then(() => setIsFullscreen(false));
+    }
+  }, []);
+
+  // Prevent body scrollbars / margins
+  useEffect(() => {
+    document.body.style.margin = "0";
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.margin = "";
+      document.body.style.overflow = "";
+    };
+  }, []);
+
+  return (
+    <div
+      onClick={toggleFullscreen}
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "black",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        cursor: isFullscreen ? "none" : "pointer",
+        zIndex: 9999, // stay above any footer
+      }}
+    >
+      <img
+        src="/bfs.png"
+        alt="Fullscreen"
+        style={{ width: "100%", height: "100%", objectFit: "contain" }}
+      />
+    </div>
+  );
+};
+
+export default FullscreenImage;
