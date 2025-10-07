04d0410750e3d40d381223b79221b10bdd69569b
pranavisda1 <pranavisda1@gmail.com>
2025-10-07 13:10:59 +0530

    new

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -55,7 +55,7 @@ export default function Navbar() {
                 transition={{ type: "spring", stiffness: 300 }}
               >
                 <Image
-                  src="https://gradient-content-server.vercel.app/content/Logo.png"
+                  src="https://gradient-content-server.vercel.app/content/logo.png"
                   alt="Gradient Logo"
                   width={92}
                   height={92}
